338a8d42e5552418f1affaa5451dd471a027de95
Raquel Yates <raquelyates@Raquels-MacBook-Air.local>
2020-09-09 17:31:14 -0700

    app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+# named the Flask app or instance (singular version)
+app = Flask(__name__)
+
+# create the root or starting point
+#@app.route('/')
+#def hello_world():
+    #return 'Hello world'
+
+#@app.route('/')
+#def I_Love_you():
+    #return 'I Love You'
